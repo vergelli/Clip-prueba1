9acab3d4523776469ac3aba71a5550c9e01fffbf
Federico <federico_joelvergelli@hotmail.com>
2020-09-01 23:40:34 -0300

    Acertijo y registro mejorado; Agregue pagina de registro de datos historicos para consultar

--- registro-scripts.js
+++ registro-scripts.js
@@ -4,16 +4,29 @@ function registrado(){
 
     var nombre = document.getElementById("nombre");
     var apellido = document.getElementById("apellido");
-    var contrasena = document.getElementById("exampleInputEmail1");
-    var email = document.getElementById("exampleInputPassword1");
+    var contrasena = document.getElementById("exampleInputPassword1");
+    var email = document.getElementById("exampleInputEmail1");
+
+    var email_s = email.value
 
     if (nombre.value != "" && apellido.value != "" && contrasena.value != "" && email.value != ""){
+        
+        if (email_s.indexOf("@") == -1){
+            
+            document.getElementById("aviso").innerHTML = 
+            `<div class="alert alert-warning">
+                <strong>Caracteres incorrectos<br></strong> El email debe tener @ </a>.
+            </div>`
 
-        document.getElementById("aviso").innerHTML = 
-        `<div class="alert alert-success">
-            <strong>Registro completado!</strong>.
-        </div>`
-        ;
+        }
+        else{
+
+            document.getElementById("aviso").innerHTML = 
+            `<div class="alert alert-success">
+                <strong>Registro completado!</strong>.
+            </div>`
+            ;
+        }
     }
     else{
 
@@ -22,4 +35,6 @@ function registrado(){
         <strong>Campos de registro vacios<br></strong>No se detectaron valores en los campos de registro
     </div>`;
     }
+
+
 }
